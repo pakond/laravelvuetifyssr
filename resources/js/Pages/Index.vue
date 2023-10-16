e1e806d4c3291909dcd1fb8dfe82af21c7eb4af6
Pau Albert Gandul <palbert@benchmarking30.com>
2023-10-16 15:50:43 +0200

    some vuetify components

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -11,6 +11,47 @@ import { ref } from 'vue';
     <div>
         Hello Laravel + InertiaJS + Vue + Vuetify in server side rendering
     </div>
+    Hover
+    <v-hover>
+    <template v-slot:default="{ isHovering, props }">
+        <v-card
+        v-bind="props"
+        :color="isHovering ? 'primary' : undefined"
+        title="Hover over me"
+        text="..."
+        ></v-card>
+    </template>
+    </v-hover>
+    <div class="text-center">
+        Progress circular
+    <v-progress-circular
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
+
+    <v-progress-circular
+      indeterminate
+      color="red"
+    ></v-progress-circular>
+
+    <v-progress-circular
+      indeterminate
+      color="purple"
+    ></v-progress-circular>
+
+    <v-progress-circular
+      indeterminate
+      color="green"
+    ></v-progress-circular>
+
+    <v-progress-circular
+      indeterminate
+      color="amber"
+    ></v-progress-circular>
+  </div>
+    Progress line
+    <v-progress-linear model-value="20"></v-progress-linear>
+    Snackbar
     <v-btn
       @click="snackbar = true"
     >
@@ -31,7 +72,26 @@ import { ref } from 'vue';
         </v-btn>
       </template>
     </v-snackbar>
+    Color picker
     <v-color-picker></v-color-picker>
+    Carousel
+    <v-carousel>
+    <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+    ></v-carousel-item>
+
+    <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+        cover
+    ></v-carousel-item>
+
+    <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        cover
+    ></v-carousel-item>
+    </v-carousel>
+    Timeline
     <v-timeline direction="horizontal">
     <v-timeline-item>
       <template v-slot:opposite>
@@ -69,6 +129,7 @@ import { ref } from 'vue';
       </div>
     </v-timeline-item>
   </v-timeline>
+  Parallax
     <v-parallax
     src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
   >
